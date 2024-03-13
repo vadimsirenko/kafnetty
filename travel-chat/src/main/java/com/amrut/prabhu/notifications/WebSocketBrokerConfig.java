00477ce7e0487e167fc188c59130664a482e5074
Vadim Sirenko <vasire@mail.ru>
2024-03-13 20:10:18 +0300

    add session dependensies

--- travel-chat/src/main/java/com/amrut/prabhu/notifications/WebSocketBrokerConfig.java
+++ travel-chat/src/main/java/com/amrut/prabhu/notifications/WebSocketBrokerConfig.java
@@ -2,28 +2,31 @@ package com.amrut.prabhu.notifications;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.session.Session;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
-import org.springframework.web.socket.handler.WebSocketHandlerDecorator;
-import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
-
+public class WebSocketBrokerConfig //implements WebSocketMessageBrokerConfigurer {
+        extends AbstractSessionWebSocketMessageBrokerConfigurer<Session>
+{
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/all","/specific");
         config.setApplicationDestinationPrefixes("/app");
     }
 
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//         registry.addEndpoint("/ws");
+//         registry.addEndpoint("/ws").withSockJS();
+//    }
+
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-         registry.addEndpoint("/ws");
-         registry.addEndpoint("/ws").withSockJS();
+    protected void configureStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws");
+        registry.addEndpoint("/ws").withSockJS();
     }
 }
